151258cb399c5cbf456c3a15176bacee7d670927
Virendra Vishwakarma <virendravishwakarma97@gmail.com>
2026-07-14 12:01:59 -0500

    login page is done

--- frontend/voyage-ai/src/app/features/auth/login/login.ts
+++ frontend/voyage-ai/src/app/features/auth/login/login.ts
@@ -1,10 +1,144 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import { Router, RouterLink } from '@angular/router';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterLink,
+
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class Login {}
+export class Login {
+
+  //==================================
+  // Dependency Injection
+  //==================================
+
+  private fb = inject(FormBuilder);
+
+  private router = inject(Router);
+
+  //==================================
+  // Signals
+  //==================================
+
+  hidePassword = signal(true);
+
+  loading = signal(false);
+
+  //==================================
+  // Login Form
+  //==================================
+
+  loginForm = this.fb.nonNullable.group({
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    ],
+
+    rememberMe: [false]
+
+  });
+
+  //==================================
+  // Toggle Password
+  //==================================
+
+  togglePassword(): void {
+
+    this.hidePassword.update(value => !value);
+
+  }
+
+  //==================================
+  // Login
+  //==================================
+
+  login(): void {
+
+    if (this.loginForm.invalid) {
+
+      this.loginForm.markAllAsTouched();
+
+      return;
+
+    }
+
+    this.loading.set(true);
+
+    console.log('Login Request');
+
+    console.log(this.loginForm.getRawValue());
+
+    // TODO:
+    // AuthService.Login()
+
+    setTimeout(() => {
+
+      this.loading.set(false);
+
+      this.router.navigate(['/dashboard']);
+
+    },1000);
+
+  }
+
+  //==================================
+  // Google Login
+  //==================================
+
+  loginWithGoogle(): void{
+
+    console.log('Google Login');
+
+  }
+
+  //==================================
+  // Apple Login
+  //==================================
+
+  loginWithApple(): void{
+
+    console.log('Apple Login');
+
+  }
+
+}
